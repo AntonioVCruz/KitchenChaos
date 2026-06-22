67e80a9cf66a720ab98a03513f9b0776265f8a26
AntonioVCruz <54753874+AntonioVCruz@users.noreply.github.com>
2026-06-22 11:16:20 -0300

    Player animation

---
+++ Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+    private const string IS_WALKING = "IsWalking";
+
+    [SerializeField] private Player player;
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        animator.SetBool(IS_WALKING, player.IsWalking());
+    }
+}
